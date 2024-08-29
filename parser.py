c10064fde47705460417acd141f48c7055dadc03
Ahmed Gouda <119757067+0xgouda@users.noreply.github.com>
2024-08-29 09:44:27 +0300

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,12 +1,7 @@
 # Parses the HTML and Creates the DOM
 
 from nodes import Text, Element
-
-SELF_CLOSING_TAGS = [
-    "area", "base", "br", "col", "embed", "hr", "img",
-    "input", "link", "meta", "param", "source", "track",
-    "wbr"
-]   
+from config import SELF_CLOSING_TAGS
 
 def print_tree(node, indent=0):
     print(" " * indent, node)
@@ -24,7 +19,7 @@ class HTMLParser:
         "link", "meta", "title", "style", "script",
     ]
 
-    # fix bas written html
+    # fix bad written html
     def implicit_tags(self, tag):
         while True:
             open_tags = [node.tag for node in self.unfinished]
@@ -52,18 +47,25 @@ class HTMLParser:
     def add_tag(self, tag):
         tag, attributes = self.get_attributes(tag)
 
+        # end the last opened tag
+        def finish_tag():
+            node = self.unfinished.pop()
+            parent = self.unfinished[-1]
+            parent.children.append(node)
+
         if tag.startswith("!"): return # throughs out comments and DOCTYPE
         self.implicit_tags(tag)
         if tag.startswith("/"):
             if len(self.unfinished) == 1: return
-            node = self.unfinished.pop()
-            parent = self.unfinished[-1]
-            parent.children.append(node)
+            finish_tag()
         elif tag in SELF_CLOSING_TAGS:
             parent = self.unfinished[-1]
             node = Element(tag, attributes, parent)
             parent.children.append(node)
         else:
+            if tag in ["p"] and tag == self.unfinished[-1].tag:
+                finish_tag()
+            
             parent = self.unfinished[-1] if self.unfinished else None
             
             node = Element(tag, attributes, parent)
@@ -86,25 +88,54 @@ class HTMLParser:
                 attributes[attrpair.casefold()] = ""
 
         return tag, attributes
+    
+    def is_comment(self, text):
+        if len(text) < 3: return False
+        if text[0:3] == "!--": return True
+        else: return False
 
     # distributes the body between Text and Tag objects
     def parse(self):
         text = ""
         in_tag = False
+        is_comment = False
+        is_script = False
+
         for c in self.body:
-            if c == "<":
-                in_tag = True
-                if text: self.add_text(text)
-                text = ""
-            elif c == ">":
-                in_tag = False
-                self.add_tag(text)
+
+            # check end of script
+            if len(text) >= 7 and text[-8:] + c == "</script>": 
+                is_script = False
+                self.add_text(text[:-8])
+                text = "/script"
+                
+            if c == "<" and not is_script:
+                is_comment = self.is_comment(text)
+                if not is_comment:
+                    in_tag = True
+                    text = text.replace("&gt;", ">").replace("&lt;", "<")
+                    if text: self.add_text(text)
+                    text = ""
+
+            # won't enter if script to treat all as text
+            elif c == ">" and not is_script:
+                is_comment = self.is_comment(text)
+                if not is_comment:
+                    in_tag = False
+                    text = text.replace("&gt;", ">").replace("&lt;", "<")
+                    self.add_tag(text)
+                    text = ""
+
+                    # check if script or not
+                    try:
+                        if self.unfinished[-1].tag == "script": is_script = True
+                    except: pass
+
+            elif len(text) >= 6 and text[-2:] == "--" and is_comment:
+                is_comment = False
                 text = ""
             else: 
                 text += c
-                text = text.replace("&gt;", ">")
-                text = text.replace("&lt;", "<")
-                text = text.replace("&shy;", '\u00AD')
         
         if not in_tag and text:
             self.add_text(text)
