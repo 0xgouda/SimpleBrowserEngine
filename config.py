50619e5cab601e617c1048ca87a5eebedbfbf6bc
Ahmed Gouda <119757067+0xgouda@users.noreply.github.com>
2024-08-29 09:44:21 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,3 +2,9 @@
 
 WIDTH, HEIGHT = 800, 600
 HSTEP, VSTEP = 13, 18
+
+SELF_CLOSING_TAGS = [
+    "area", "base", "br", "col", "embed", "hr", "img",
+    "input", "link", "meta", "param", "source", "track",
+    "wbr"
+]
