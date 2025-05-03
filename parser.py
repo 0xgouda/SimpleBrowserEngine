5f9c4c004745c574610befad815dc58519501bc8
0xgouda <ahmedabdelmonaem@std.mans.edu.eg>
2025-05-03 03:24:01 +0300

    fix: ignore nested <p> tags
    this includes also minor changs that enhances readability & performance

--- parser.py
+++ parser.py
@@ -8,7 +8,6 @@ def print_tree(node, indent=0):
     for child in node.children:
         print_tree(child, indent + 2)
 
-# constructs the document tree
 class HTMLParser:
     def __init__(self, body):
         self.body = body
@@ -25,12 +24,12 @@ class HTMLParser:
             open_tags = [node.tag for node in self.unfinished]
             if open_tags == [] and tag != "html":
                 self.add_tag("html")
-            elif open_tags == ["html"] and tag not in ["head", "body", "/html"]:
+            elif open_tags[-1] == "html" and tag not in ["head", "body", "/html"]:
                 if tag in self.HEAD_TAGS:
                     self.add_tag("head")
                 else:
                     self.add_tag("body")
-            elif open_tags == ["html", "head"] and tag not in ["/head"] + self.HEAD_TAGS:
+            elif open_tags[-1] == "head" and tag not in self.HEAD_TAGS:
                 self.add_tag("/head")
             else:
                 break
@@ -46,8 +45,8 @@ class HTMLParser:
     # creates the tag node
     def add_tag(self, tag):
         tag, attributes = self.get_attributes(tag)
+        unfinished_len = len(self.unfinished)
 
-        # end the last opened tag
         def finish_tag():
             node = self.unfinished.pop()
             parent = self.unfinished[-1]
@@ -56,17 +55,17 @@ class HTMLParser:
         if tag.startswith("!"): return # throughs out comments and DOCTYPE
         self.implicit_tags(tag)
         if tag.startswith("/"):
-            if len(self.unfinished) == 1: return
+            if unfinished_len <= 1: return
             finish_tag()
         elif tag in SELF_CLOSING_TAGS:
             parent = self.unfinished[-1]
             node = Element(tag, attributes, parent)
             parent.children.append(node)
         else:
-            if tag in ["p"] and tag == self.unfinished[-1].tag:
-                finish_tag()
+            if tag == "p" and unfinished_len and tag == self.unfinished[-1].tag:
+                return
             
-            parent = self.unfinished[-1] if self.unfinished else None
+            parent = self.unfinished[-1] if unfinished_len else None
             
             node = Element(tag, attributes, parent)
             self.unfinished.append(node)
@@ -90,9 +89,7 @@ class HTMLParser:
         return tag, attributes
     
     def is_comment(self, text):
-        if len(text) < 3: return False
-        if text[0:3] == "!--": return True
-        else: return False
+        return not len(text) < 3 and text[:3] == "!--"
 
     # distributes the body between Text and Tag objects
     def parse(self):
@@ -103,35 +100,29 @@ class HTMLParser:
 
         for c in self.body:
 
-            # check end of script
             if len(text) >= 7 and text[-8:] + c == "</script>": 
                 is_script = False
                 self.add_text(text[:-8])
                 text = "/script"
                 
-            if c == "<" and not is_script:
-                is_comment = self.is_comment(text)
-                if not is_comment:
-                    in_tag = True
-                    text = text.replace("&gt;", ">").replace("&lt;", "<")
-                    if text: self.add_text(text)
-                    text = ""
+            is_comment = self.is_comment(text)
 
-            # won't enter if script to treat all as text
-            elif c == ">" and not is_script:
-                is_comment = self.is_comment(text)
-                if not is_comment:
-                    in_tag = False
-                    text = text.replace("&gt;", ">").replace("&lt;", "<")
-                    self.add_tag(text)
-                    text = ""
+            if c == "<" and not is_script and not is_comment:
+                in_tag = True
+                if text: self.add_text(text)
+                text = ""
 
-                    # check if script or not
-                    try:
-                        if self.unfinished[-1].tag == "script": is_script = True
-                    except: pass
+            elif c == ">" and not is_script and not is_comment:
+                in_tag = False
+                text = text.replace("&gt;", ">").replace("&lt;", "<")
+                self.add_tag(text)
+                text = ""
 
-            elif len(text) >= 6 and text[-2:] == "--" and is_comment:
+                try:
+                    if self.unfinished[-1].tag == "script": is_script = True
+                except: pass
+
+            elif text[-2:] + c == "-->" and is_comment:
                 is_comment = False
                 text = ""
             else: 
@@ -144,8 +135,6 @@ class HTMLParser:
 
     # finishes the unclosed tags and returns the father node
     def finish(self):
-        if not self.unfinished:
-            self.implicit_tags(None)
         while len(self.unfinished) > 1:
             node = self.unfinished.pop()
             parent = self.unfinished[-1]
