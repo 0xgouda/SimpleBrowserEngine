71320e4199c59ca5037c16f64a40f7a804686179
0xgouda <ahmedabdelmonaem@std.mans.edu.eg>
2025-05-05 00:37:34 +0300

    refactor: cleaner rewrite of Layout module
    1. added TAG_ACTIONS global dict and apply_tag_actions method to avoid hardcoding each tag's custom layout
    2. merged recurse & view_source into a single render_node method

--- layout.py
+++ layout.py
@@ -1,4 +1,4 @@
-# processes all the page text and saves it to a list
+# processes all the page the body view and saves it to a list
 
 import tkinter.font
 from nodes import Text
@@ -10,14 +10,22 @@ def get_font(size, weight, style):
     key = (size, weight, style)
     if key not in FONTS:
         font = tkinter.font.Font(size=size, weight=weight, slant=style)
-        # label object speeds the .measure() method
-        label = tkinter.Label(font=font)
-        FONTS[key] = (font, label)
+        tkinter.Label(font=font)
+        FONTS[key] = font
     
-    return FONTS[key][0]
+    return FONTS[key]
+
+TAG_ACTIONS = {
+    "i": {"style": "italic"},
+    "b": {"weight": "bold"},
+    "small": {"size": -2},
+    "big": {"size": 4},
+    "br": {"flush": True},
+    "h1": {"flush": True, "size": 4, "weight": "bold"}
+}
 
 class Layout:
-    def __init__(self, tree, view_source):
+    def __init__(self, dom_root, view_source_enabled):
         self.display_list = []
         self.cursor_x = config.HSTEP
         self.cursor_y = config.VSTEP
@@ -25,128 +33,83 @@ class Layout:
         self.style = "roman"
         self.size = 12
         self.line = []
-        self.title = False
+        self.view_source_enabled = view_source_enabled
 
-        # initializes the loop over the nodes tree
-        if view_source: self.view_source(tree)
-        else: self.recurse(tree)
-        
-        # final one for the incompelete line
-        self.flush()
+        self.render_node(dom_root)
+        self.flush_line_buffer()
     
-    # handles the (bold, italic, ...etc) text formatting tags and the line breaks
-    def open_tag(self, tag):
-        if tag == "i":
-            self.style = "italic"
-        elif tag == "b":
-            self.weight = "bold"
-        elif tag == "small":
-            self.size -= 2
-        elif tag == "big":
-            self.size += 4
-        elif tag == "br":
-            self.flush() 
-        elif tag == "h1" and self.title:
-            self.flush()
-            self.size += 4
-            self.weight = "bold"
+    def apply_tag_actions(self, tag):
+        if tag not in TAG_ACTIONS: return 
+        
+        old_values = {}
+        for key, value in TAG_ACTIONS[tag].items():
+            if key == "flush":
+                self.flush_line_buffer()
+                continue
 
-    # reverts the changes made by open_tag
-    def close_tag(self, tag):
-        if tag == "i":
-            self.style = "roman"
-        elif tag == "b":
-            self.weight = "normal"
-        elif tag == "small":
-            self.size += 2
-        elif tag == "big":
-            self.size -= 4
-        elif tag == "p":
-            self.flush()
-            self.cursor_y += config.VSTEP
-        elif self.title and tag == "h1":
-            self.flush()
-            self.title = False
-            self.size -= 2
-            self.weight = "normal"
-        elif tag == "p":
-            self.flush()
-            self.cursor_y += config.VSTEP
+            if isinstance(value, int):
+                value += self.key
 
-    # goes over the page nodes to render them in the display_list 
-    def recurse(self, tree):
-        if isinstance(tree, Text):
-            for word in tree.text.split():
+            old_values[key] = self.key
+            setattr(self, key, value)
+
+        yield
+
+        for key in TAG_ACTIONS[tag]:
+            setattr(self, key, old_values[key])
+         
+    def render_node(self, node):
+        if isinstance(node, Text):
+            for word in node.text.split():
                 self.word(word)
-        else:
-            if tree.attributes.get("class") == "title" and tree.tag == "h1":
-                self.title = True
+            return
 
-            self.open_tag(tree.tag)
-            for child in tree.children:
-                self.recurse(child)
-            self.close_tag(tree.tag)
+        if not self.view_source_enabled:
+            self.apply_tag_actions(node.tag)
+            for child in node.children:
+                self.render_node(child)
+            self.apply_tag_actions(node.tag)
+            return
 
-    # sets the metrics for the words in the line buffer
-    def flush(self):
+        attributes = ''
+        for key, value in node.attributes.items():
+            attributes += f'{key}="{value}"'
+
+        self.flush_line_buffer()
+        self.word(f"<{node.tag} {attributes}>")
+        self.flush_line_buffer()
+
+        for child in node.children:
+            self.render_node(child)
+        
+        if node.tag not in config.SELF_CLOSING_TAGS:
+            self.flush_line_buffer()
+            self.word('</' + node.tag + '>')
+            self.flush_line_buffer()
+
+    def flush_line_buffer(self):
         if not self.line: return
-        # calculates the baseline where all words should be on
+
         metrics = [font.metrics() for x, word, font in self.line]
         max_ascent = max([metric["ascent"] for metric in metrics])
-        baseline = self.cursor_y + 1.25 * max_ascent
+        top_baseline = self.cursor_y + 1.25 * max_ascent
 
-        # Centers the title
-        measures = 0
-        if self.title:
-            measures = sum([font.measure(word) for x, word, font in self.line])
-
-        # sets the y for each word to be 
         for x, word, font in self.line:
-            y = baseline - font.metrics("ascent")
-            self.display_list.append((((x + ((config.WIDTH - measures) / 2)) if self.title else x), y, word, font))
+            y = top_baseline - font.metrics("ascent")
+            self.display_list.append((x, y, word, font))
 
         max_descent = max([metric["descent"] for metric in metrics])
-        self.cursor_y = baseline + 1.25 * max_descent
+        self.cursor_y = top_baseline + 1.25 * max_descent
         self.cursor_x = config.HSTEP
         self.line = []
     
-    # displays the page source with tags being in bold
-    # Note: attributes values with spaces class="bla bla" will have weird output as they are not supported
-    def view_source(self, tree):
-        if isinstance(tree, Text):
-            self.weight = "bold"
-            for word in tree.text.split():
-                self.word(word)
-            self.weight = "normal"
-        else:
-            attributes = ''
-            for key in tree.attributes:
-                if key == '/': 
-                    attributes += '/'
-                    continue
-
-                attributes += key + '='
-                attributes += '"' + tree.attributes.get(key) + '" ' 
-
-            self.flush()
-            self.word('<' + tree.tag + ' ' + attributes + '>')
-            self.flush()
-
-            for child in tree.children:
-                self.view_source(child)
-            
-            if tree.tag not in config.SELF_CLOSING_TAGS:
-                self.flush()
-                self.word('</' + tree.tag + '>')
-                self.flush()
-
-    # determines the shape and coordinates of the letter
+    # determines the font and x-cords of the word
     def word(self, word):
         font = get_font(self.size, self.weight, self.style)
         w = font.measure(word)
 
         if self.cursor_x + w > config.WIDTH - config.HSTEP:
-            self.flush()
+            self.flush_line_buffer()
         else:
             self.line.append((self.cursor_x, word, font))
             self.cursor_x += w + font.measure(" ")
