de901277231e37553b3f1d7c19389f5dc1a8ea4d
0xgouda <ahmedabdelmonaem@std.mans.edu.eg>
2025-05-06 00:46:01 +0300

    refactor: use urlib to parse urls & requests to send GET requests

--- urls.py
+++ urls.py
@@ -1,146 +1,43 @@
-import socket
-import ssl
-import time
+from urllib.parse import urlparse, urljoin, parse_qs
+import os
+import requests
 
 # handles the url parsing, connection creation, sending request and returns the response
 class URL:
-    MAX_REDIRECTS = 5
+    SUPPORTED_DATA_SCHEME_FORMATS = ["text/html"]
 
-    def __init__(self, url, redirect_count=0):
-        try:
-            # future refactor: remove this to allow for captial query params data
-            url = url.lower()
+    def __init__(self, url: str):
+        self.view_source_enabled = False
+        if url.startswith("view-source"):
+            self.view_source_enabled = True     
+            url = url[len('view-source:'):]
+        
+        self.opened_file = None
 
-            self.view_source_enabled = False
-            if url[:12] == "view-source:":            
-                self.view_source_enabled = True
-                url = url[12:]
+        parsed_url = urlparse(url)
 
-            self.redirect_count = redirect_count
-            self.cache = {}
-            self.file = None
+        self.scheme = parsed_url.scheme
+        self.path = parsed_url.path
 
-            self.scheme, url = url.split(":", 1)
-            if self.scheme == "data":
-                self.mediatype, self.data = url.split(",", 1)
-                return
-            url = url[2:]
-            
-            if self.scheme == "file":
-                self.file_name = url
-                return
-
-            self.host, self.path = url.split("/", 1)
-            self.path += "/" if self.path != "" else ""
-
-            if self.scheme == "http":
-                self.port = 80
-            elif self.scheme == "https":
-                self.port = 443
-
-            if ":" in self.host:
-                self.host, port = self.host.split(":", 1)
-                self.port = int(port)
-        except:
+        if self.scheme and parsed_url.netloc:
+            self.url = urljoin(url, self.path)
+            self.query_params = parse_qs(parsed_url.query)
+        elif self.scheme == "file" and os.path.isfile(self.path):
+            self.file_name = self.path
+        elif self.scheme == "data" and ',' in self.path and any(self.path.split(',', 1)[0].startswith(fmt) for fmt in self.SUPPORTED_DATA_SCHEME_FORMATS):
+            self.mediatype, self.data = self.path.split(',', 1)
+        else:
             self.scheme, self.path = "about", "blank"
-    
-    def create_socket(self):
-        s = socket.socket(
-            family = socket.AF_INET,
-            type = socket.SOCK_STREAM,
-            proto = socket.IPPROTO_TCP
-        )
-        s.connect((self.host, self.port))
-        
-        if self.scheme == "https":
-            ctx = ssl.create_default_context()
-            s = ctx.wrap_socket(s, server_hostname=self.host)
-        
-        return s
 
-    def redirect(self, response_headers):
-        if self.redirect_count > self.MAX_REDIRECTS:
-                return "Error due to too much redirects"
-        self.redirect_count += 1
-
-        location = response_headers.get("location")
-        if location[0] == "/":
-            self.path = location
-        else:
-            self.__init__(location, self.redirect_count)
-        return self.request()
-
-    def cache_reponse(self, key, status_line, content, response_headers):
-        if status_line.split(" ", 2)[1] == "200":
-            if "cache-control" in response_headers:
-                cache_control = response_headers["cache-control"].strip().lower()
-                directives = cache_control.split(",")
-                if 'no-store' in directives:
-                    return
-
-                if 'max-age=' in directives:
-                    try:
-                        max_age = int(cache_control.split("=", 1)[1])
-                    except ValueError:
-                        max_age = 7200
-                
-            expiration_date = time.time() + max_age
-            self.cache[key] = {"status_line" : status_line, "content": content, "response_headers": response_headers, "expiration_date": expiration_date}
-
-    def get_cached_response(self, key):
-        if key not in self.cache:
-            return None
-
-        cached_response = self.cache[key]
-        expiration_date = cached_response.get("content")
-        if time.time() < expiration_date:
-            return cached_response
-        else:
-            del self.cache[key]
-
-    # TODO: handle socket closing logic
     def request(self):
-        key = self.scheme + "://" + self.host + self.path
-        cached_response = self.get_cached_response(key)
-        if cached_response == None:
-            socket = self.create_socket()
+        response = requests.get(self.url, params=self.query_params)
+        return response.content.decode()
 
-            request = f"GET {self.path} HTTP/1.1\r\n"
-            request += f"Host: {self.host}\r\n"
-            request += "User-Agent: gouda's browser\r\n"
-            request += "\r\n"
-            socket.send(request.encode("utf8"))
-
-            response = socket.makefile("r", encoding="utf-8", newline="\r\n")
-            
-            statusline = response.readline()
-            _, status_code, _ = statusline.split(" ", 2)
-            
-            response_headers = {}
-            while True:
-                line = response.readline()
-                if line == "\r\n": break
-                header, value = line.split(":", 1)
-                response_headers[header.casefold()] = value.strip()
-
-            content = response.read(int(response_headers.get("content-length", 0)))
-            socket.close()
-
-            self.cache_reponse(key, statusline, content, response_headers)
-        else:
-            content = cached_response["content"]
-            _, status_code, _ = cached_response["status_line"].split(" ", 2)
-
-        if int(status_code) >= 300 and int(status_code) < 400:
-            return self.redirect(response_headers)
-        else:
-            return content
-
-    def close(self):
-        if self.file:
-            self.file.close()
-            self.file = None
+    def close_file(self):
+        if self.opened_file:
+            self.opened_file.close()
+            self.opened_file = None
 
     def open_file(self):
-        self.file = open(self.file_name, 'r')
-        return self.file.read()
+        self.opened_file = open(self.file_name, 'r')
+        return self.opened_file.read()
